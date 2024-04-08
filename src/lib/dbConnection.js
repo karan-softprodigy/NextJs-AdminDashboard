75344745ae81ac7f459d1f1c92ec15bf1e33379b
karan-softprodigy <karandeep_poddar@softprodigy.com>
2024-04-08 19:08:35 +0530

    mongodb pass changed

--- src/lib/dbConnection.js
+++ src/lib/dbConnection.js
@@ -3,16 +3,16 @@ import mongoose from "mongoose";
 
 export default async () => {
   try {
-    const DB_OPTIONS = {
-      dbName: process.env.DB_NAME,
-      // key: process.env.DATA_API_KEY,
-      // apiKey: process.env.DATA_API_KEY,
-      // apiURL: process.env.DATA_API_URL,
-      // user: process.env.DB_USERNAME,
-      // pass: process.env.DB_PASSWORD,
-      // authSource: process.env.DB_AUTH_SOURCE,
-    };
-    await mongoose.connect(process.env.MONGODB_URI, DB_OPTIONS);
+    // const DB_OPTIONS = {
+    //   // dbName: process.env.DB_NAME,
+    //   // key: process.env.DATA_API_KEY,
+    //   // apiKey: process.env.DATA_API_KEY,
+    //   // apiURL: process.env.DATA_API_URL,
+    //   user: process.env.DB_USERNAME,
+    //   pass: process.env.DB_PASSWORD,
+    //   authSource: process.env.DB_AUTH_SOURCE,
+    // };
+    await mongoose.connect(process.env.MONGODB_URI);
     // await mongoose.connect(process.env.DB_URL, DB_OPTIONS);
 
     console.log("DB connected successfully...");
